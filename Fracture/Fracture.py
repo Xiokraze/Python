a3192a47f39752626b7d32111a50769e6c77ce42
Xiokraze <xiokraze@gmail.com>
2019-09-07 09:35:16 -0400

    Completed sprite implementation for borders, player, screen, and sphere classes from original to new project.

--- Fracture/Fracture.py
+++ Fracture/Fracture.py
@@ -1,5 +1,7 @@
 import math
 import sys
+import random
+from PIL import ImageFont
 import pygame
 pygame.mixer.pre_init(44100, -16, 2, 2048)
 pygame.mixer.init()
@@ -10,9 +12,8 @@ class Border(pygame.sprite.Sprite):  # TODO refactor
     def __init__(self, screen_obj, border_position):
         super().__init__()
         self.side = border_position
-        top_padding = 200
         side_padding = screen_obj.x_min
-        side_height = screen_obj.screen_height - top_padding
+        side_height = screen_obj.screen_height - screen_obj.top_padding
         self.border_width = screen_obj.border_width
         border_width = self.border_width
         border_color = (0, 0, 255)
@@ -26,41 +27,19 @@ class Border(pygame.sprite.Sprite):  # TODO refactor
             else:
                 x = screen_obj.screen_width - border_width - side_padding
                 self.rect.x = x
-            self.rect.y = top_padding
-        else:
+            self.rect.y = screen_obj.top_padding
+
+        if self.side == "top" or self.side == "bot":
             screen_width = screen_obj.screen_width
             width = screen_width - side_padding * 2 - self.border_width * 2
             self.image = pygame.Surface([width, border_width])
             self.image.fill(border_color)
             self.rect = self.image.get_rect()
             self.rect.x = 0 + side_padding + self.border_width
-            self.rect.y = top_padding
-
-
-class Sphere(pygame.sprite.Sprite):
-    def __init__(self, screen_obj):
-        super().__init__()
-        self.image = pygame.image.load("Media/spheres/dark_blue.png")
-        self.speed = 5
-        self.speed_x = self.speed
-        self.speed_y = self.speed
-        self.angle = 45
-        self.size = self.image.get_size()
-        self.rect = self.image.get_rect()
-        self.rect.x = screen_obj.screen_width / 2 - self.size[0] / 2
-        self.rect.y = screen_obj.screen_height - self.size[1] * 10
-
-    def move(self):
-        radians = math.radians(self.angle)
-        self.rect.x += self.speed_x * math.sin(radians)
-        self.rect.y -= self.speed_y * math.cos(radians)
-        return
-
-    def update(self, game):
-        game.border_collision()
-        game.player_collision()
-        self.move()
-        return
+            if self.side == "top":
+                self.rect.y = screen_obj.top_padding
+            else:
+                self.rect.y = screen_obj.screen_height
 
 
 class Player(pygame.sprite.Sprite):
@@ -75,6 +54,16 @@ class Player(pygame.sprite.Sprite):
         self.x_min = screen_obj.x_min
         self.x_max = screen_obj.x_max
         self.border_padding = screen_obj.border_width
+
+    def get_segments(self):
+        segments = []
+        x = self.rect.x
+        width = self.size[0]
+        segment_width = width / 4
+        for i in range(4):
+            segments.append((x, x + segment_width))
+            x += segment_width
+        return segments
 
     def move_left(self):
         x = self.rect.x - self.speed - self.border_padding
@@ -113,6 +102,7 @@ class Screen(object):
         self.title = "Fracture"
         self.screen_width = 800
         self.screen_height = 800
+        self.top_padding = 200
         self.x_min = 50
         self.x_max = self.screen_width - self.x_min
         self.border_width = 20
@@ -125,24 +115,106 @@ class Screen(object):
         return screen
 
 
+class Sphere(pygame.sprite.Sprite):
+    def __init__(self, screen_obj):
+        super().__init__()
+        self.image = pygame.image.load("Media/spheres/dark_blue.png")
+        self.speed = 5
+        self.speed_x = self.speed
+        self.speed_y = self.speed * -1
+        self.angle = 45
+        self.size = self.image.get_size()
+        self.rect = self.image.get_rect()
+        self.rect.x = screen_obj.screen_width / 2 - self.size[0] / 2
+        self.rect.y = screen_obj.screen_height - self.size[1] * 10
+        self.x_min = screen_obj.x_min + screen_obj.border_width
+        self.x_max = screen_obj.screen_width - screen_obj.x_max - self.size[0]
+        self.y_max = screen_obj.top_padding + screen_obj.border_width + self.size[1]
+
+    def set_border_deflection_angle(self, border):
+        if border.side == "left":
+            self.angle = 360 - self.angle
+        if border.side == "right":
+            self.angle = 360 - self.angle
+        if border.side == "top":
+            self.angle = (180 - self.angle) % 360
+        if border.side == "bot":
+            pass  # Place holder for sphere/bottom border collision handling
+        return
+
+    def new_angle(self, player_segments):
+        segment_num = 0
+        for start, end in player_segments:
+            if start <= self.rect.x < end:
+                break
+            segment_num += 1
+        if segment_num == 0:
+            self.angle = 300
+        elif segment_num == 1:
+            self.angle = 340
+        elif segment_num == 2:
+            self.angle = 20
+        elif segment_num == 3:
+            self.angle = 60
+        return
+
+    def move(self):
+        radians = math.radians(self.angle)
+        move_x = int(self.speed_x * math.sin(radians))
+        move_y = int(self.speed_y * math.cos(radians))
+        self.rect.y += move_y
+        self.rect.x += move_x
+        return
+
+    def update(self, game):
+        game.border_collision()
+        game.player_collision()
+        self.move()
+        return
+
+
 class Game(object):
     def __init__(self):
         self.screen_obj = Screen()
         self.sphere_sprites = pygame.sprite.RenderUpdates()
         self.border_sprites = pygame.sprite.RenderPlain()
         self.player_sprites = pygame.sprite.RenderUpdates()
+        self.all_sprites = pygame.sprite.Group()
 
         # Time Handling
         self.clock = pygame.time.Clock()
         self.max_FPS = 60
         self.frame_count = 0
         self.seconds = 0
+        self.blink_frame_count = 0
+        self.blink_delay = .5
+        self.blinking = True
 
-        return
+        # Prompts
+        self.title_prompt = "PRESS ENTER"
+
+        # Fonts/Colors
+        self.master_font = "Media/ariblk.ttf"
+        self.font_size = 20
+        self.text_color = (255, 255, 255)
+        self.font = ImageFont.truetype(self.master_font, self.font_size)
+        self.word_font = pygame.font.Font(self.master_font, self.font_size)
+
+        # Media
+        self.game_background = pygame.image.load("Media/backgrounds/space1.png")
+        self.title_image = pygame.image.load("Media/title_image.png")
 
     #####################
     #    Game Control   #
     #####################
+    def update_seconds(self):
+        self.blink_frame_count += 1
+        blink_frame_count = self.max_FPS * self.blink_delay
+        if self.blink_frame_count == blink_frame_count:
+            self.blink_frame_count = 0
+            return True
+        return False
+
     def check_frame_count(self):
         if self.frame_count >= self.max_FPS:
             self.frame_count = 0
@@ -182,13 +254,32 @@ class Game(object):
         self.border_sprites.add(Border(self.screen_obj, "left"))
         self.border_sprites.add(Border(self.screen_obj, "right"))
         self.border_sprites.add(Border(self.screen_obj, "top"))
+        self.border_sprites.add(Border(self.screen_obj, "bot"))
+        return
+
+    def get_all_sprites(self):
+        for border in self.border_sprites:
+            self.all_sprites.add(border)
+        for sphere in self.sphere_sprites:
+            self.all_sprites.add(sphere)
+        for player in self.player_sprites:
+            self.all_sprites.add(player)
         return
 
     def get_sprites(self):
         self.get_border_sprites()
         self.sphere_sprites.add(Sphere(self.screen_obj))
         self.player_sprites.add(Player(self.screen_obj))
+        self.get_all_sprites()
         return
+
+    def get_sphere_border_collision(self, sphere):
+        border_collision = pygame.sprite.spritecollide(
+            sphere,
+            self.border_sprites,
+            False
+        )
+        return border_collision
 
     #####################
     #     Collision     #
@@ -202,34 +293,70 @@ class Game(object):
         )
         for sphere in collisions:
             player = collisions[sphere][0]
-            # TODO complete player collision
+            segments = player.get_segments()
+            sphere.new_angle(segments)
         return
 
     def border_collision(self):
-        collisions = pygame.sprite.groupcollide(
-            self.sphere_sprites,
-            self.border_sprites,
-            0,
-            0
-        )
-        for sphere in collisions:
-            border = collisions[sphere][0]
-            if border.side == "left" or border.side == "right":
-                sphere.speed_x *= -1
-            if border.side == "top":
-                if sphere.speed_y > 0:
-                    sphere.speed_y *= -1
+        for sphere in self.sphere_sprites:
+            border_collision = self.get_sphere_border_collision(sphere)
+            if border_collision:
+                border = border_collision[0]
+                if border.side == "bot":
+                    sphere.kill()
+                    continue
+                sphere.set_border_deflection_angle(border)
         return
 
     def play(self):
         self.get_sprites()
         while self.continue_game():
-            self.screen_obj.screen.fill((0, 0, 0))
-            self.border_sprites.draw(self.screen_obj.screen)
-            self.sphere_sprites.draw(self.screen_obj.screen)    
-            self.player_sprites.draw(self.screen_obj.screen)
-            self.sphere_sprites.update(self)
+            self.draw_game_background()
+            self.all_sprites.draw(self.screen_obj.screen)
             self.player_sprites.update()
+            self.sphere_sprites.update(self)
+            pygame.display.update()
+        return
+
+    #####################
+    #    Title Screen   #
+    #####################
+    def blink_text(self):
+        if self.update_seconds():
+            if self.blinking:
+                self.blinking = False
+                return True
+            else:
+                self.blinking = True
+        if self.blinking:
+            self.draw_blink_text(self.title_prompt)
+        return False
+
+    def draw_blink_text(self, prompt):
+        text = self.word_font.render(prompt, 1, self.text_color)
+        size = self.font.getsize(prompt)
+        x = self.screen_obj.screen_width / 2 - size[0] / 2
+        y = self.screen_obj.screen_height / 2 + size[1]
+        self.screen_obj.screen.blit(text, (x, y))
+        return
+
+    def draw_game_background(self):
+        # self.screen_obj.screen.blit(self.game_background, (0, 0))
+        self.screen_obj.screen.fill((0, 0, 0))
+        return
+
+    def draw_title_image(self):
+        size = self.title_image.get_size()
+        x = self.screen_obj.screen_width / 2 - size[0] / 2
+        y = size[1]
+        self.screen_obj.screen.blit(self.title_image, (x, y))
+        return
+
+    def title_screen(self):
+        while self.continue_game(True):
+            self.draw_game_background()
+            self.draw_title_image()
+            self.blink_text()
             pygame.display.update()
         return
 
@@ -239,7 +366,7 @@ class Game(object):
 #####################
 def main():
     game = Game()
-    # game.title_screen()
+    game.title_screen()
     game.play()
     game.quit_game()
 
